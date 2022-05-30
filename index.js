4d048e44bb9cc3bb093e09159fdf3f73ddacbcac
Lightening Gibson <lighteningibson@gmail.com>
2022-05-30 15:03:22 -0500

    commit tree ed725c91e0c40467a14055263dbbc2ffd7ac6317

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
+ 
+let divide = (num1=2000, num2=100) => num1/num2
 
-
-function divide() {
-
-    return 2000/100
-
-
-    
-}
-const square = (num1) => num1 ** 2;
-
-const add = (num1, num2) => num1 + num2;
-
+let square = (x=2) => x * x
